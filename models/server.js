0cf7904ed5b4769a88ab95c844d65ce29e326209
Erick Buitrago <erickbuitrago2014@gmail.com>
2022-03-27 22:14:43 -0500

    Emision y escucha de eventos tanto en el servidor como en el cliente

--- models/server.js
+++ models/server.js
@@ -1,11 +1,14 @@
-    const express = require('express');
+const express = require('express');
 const cors = require('cors');
+const { createServer } = require('http');
 
 class Server {
 
-    constructor () {
+    constructor() {
         this.app = express();
         this.port = process.env.PORT || 8080;
+        this.server = createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {};
 
@@ -14,22 +17,41 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        //Sockets
+        this.sockets();
     }
 
     middlewares() {
         //CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         //Directorio público
-        this.app.use( express.static('public') );
+        this.app.use(express.static('public'));
     }
 
     routes() {
         // this.app.use(this.paths.auth, require('../routes/auth'));
     }
 
+    sockets() {
+        this.io.on('connection', (socket) => {
+            console.log('Cliente conectado', socket.id);
+
+            socket.on('disconnect', () => {
+                console.log('Cliente desconectado', socket.id)
+            });
+
+            socket.on('enviar-mensaje', (payload) => {
+
+                this.io.emit('enviar-mensaje', payload);
+
+            })
+        });
+    }
+
     start() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log('Server on port', this.port);
         });
     }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,37 @@
 
+// Referencias del HTML
+const lblOnline = document.getElementById('lblOnline');
+const lblOffline = document.getElementById('lblOffline');
+const txtMensaje = document.getElementById('txtMensaje');
+const btnEnviar = document.getElementById('btnEnviar');
+
+const socket = io();
+
+// socket.on -> Sirve para escuchar un evento
+// socket.emit -> Sirve para emitir un evento
+
+socket.on('connect', () => {
+    lblOffline.style.display = "none";
+    lblOnline.style.display = "";
+});
+
+socket.on('disconnect', () => {
+    lblOnline.style.display = "none";
+    lblOffline.style.display = "";
+});
+
+socket.on('enviar-mensaje', (payload) => {
+   console.log(payload);
+});
+
+// Emitir desde el cliente - Escuchar en el servidor
+btnEnviar.addEventListener('click', () => {
+
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: socket.id
+    }
+    socket.emit('enviar-mensaje', payload);
+    
+});
